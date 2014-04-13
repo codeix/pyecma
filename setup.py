d7f966d20cfa6932b07157468b0cb52a810fcd7f
sriolo <sriolo@b094a52f-4d33-4e25-9035-2e1254a7f691>
2014-04-13 13:29:28 +0000

    add ebnf
    git-svn-id: https://svn.sams.dyns.net/private/python/pyecma/trunk@30 b094a52f-4d33-4e25-9035-2e1254a7f691

--- setup.py
+++ setup.py
@@ -21,8 +21,15 @@ setup(name='pyecma',
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python3",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Interpreters',
+        'Operating System :: OS Independent',
         ],
       keywords='Python3, Python, Javascript, ECMA, ECMAScript, grako',
       author='Samuel Riolo',
@@ -40,7 +47,7 @@ setup(name='pyecma',
       extras_require={
           'build': ['grako']
       },
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+      entry_points={
+          'console_scripts': ['pyecma = pyecma.console:run'],
+      },
       )
---
+++ src/pyecma/console.py
@@ -0,0 +1,2 @@
+def run():
+    print('parse your js')
