b522063edf359a1f7493a1c05c153b501755c743
Samuel <samuel.riolo@swissonline.ch>
2014-08-03 17:44:03 +0200

    remove unsupported python version in setup files

--- setup.py
+++ setup.py
@@ -37,10 +37,6 @@ setup(name='pyecma',
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Interpreters',
